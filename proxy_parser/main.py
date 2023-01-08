067353a888f4cf29faf9dce0d28864b19ff235c0
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-01-08 23:46:16 +0200

    commit

--- proxy_parser/main.py
+++ proxy_parser/main.py
@@ -1,16 +1,29 @@
+import os
 from pathlib import Path
 
 from proxy_parser.checkers import check_proxy_list
 from proxy_parser.config import SAVE_PATH
-from proxy_parser.parsers import get_uncheked_proxies, append_proxy_to_file
+from proxy_parser.parsers import get_uncheked_proxies, append_proxy_to_file, clean_file
 
 
 def main():
     all_unchecked_proxies = get_uncheked_proxies()
+    path_to_file = Path(SAVE_PATH, 'parsed.txt')
+    c = 0
 
     for proxy in check_proxy_list(all_unchecked_proxies):
+        if c == 0 and path_to_file.exists():
+            os.remove(path_to_file)
+            c += 1
         print(proxy)
-        append_proxy_to_file(Path(SAVE_PATH, 'parsed.txt'), proxy)
+        append_proxy_to_file(path_to_file, proxy)
+
+    clean_file(path_to_file)
+
+
+def infinite_main():
+    while True:
+        main()
 
 
 if __name__ == '__main__':
--- proxy_parser/parsers.py
+++ proxy_parser/parsers.py
@@ -1,7 +1,7 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from typing import Generator, NoReturn, Iterable
+from typing import Generator, NoReturn
 
 import requests
 
@@ -10,13 +10,11 @@ from proxy_parser.config import REGEX_PATTERN
 
 
 def append_proxy_to_file(path_to_file: Path | str, proxy: str) -> NoReturn:
-    if path_to_file.exists():
-        os.remove(path_to_file)
     with open(path_to_file, 'a') as file:
         file.write(f'{proxy}\n')
 
 
-def remove_duplicates(path_to_file: Path | str) -> NoReturn:
+def clean_file(path_to_file: Path | str) -> NoReturn:
     with open(path_to_file) as file:
         iterable = set(file.read().split('\n'))
         iterable.remove('')
