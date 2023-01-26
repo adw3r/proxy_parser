a6268ccc462b40dc52ebbdd1422d8e8b4fd30e1a
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-01-26 13:44:57 +0200

    commit

--- main.py
+++ main.py
@@ -7,21 +7,17 @@ from proxy_parser.parsers import get_uncheked_proxies, get_sources_from_github, 
 
 
 async def main():
-    query = 'filename:http_proxies.txt'
-    print(f'searching for {query}')
-    links_form_github: set = set(link for link in get_sources_from_github(15, query) if link)
-    path_to_http_sources = Path(PATH_TO_SOURCES, 'http.txt')
-    save_iterable_to_file(path_to_http_sources, set(links_form_github))
-    print(f'searching for {query}')
-    query = 'filename:socks4_proxies.txt'
-    links_form_github: set = set(link for link in get_sources_from_github(15, query) if link)
-    path_to_http_sources = Path(PATH_TO_SOURCES, 'socks4.txt')
-    save_iterable_to_file(path_to_http_sources, set(links_form_github))
-    print(f'searching for {query}')
-    query = 'filename:socks5_proxies.txt'
-    links_form_github: set = set(link for link in get_sources_from_github(15, query) if link)
-    path_to_http_sources = Path(PATH_TO_SOURCES, 'socks5.txt')
-    save_iterable_to_file(path_to_http_sources, set(links_form_github))
+    queries = {
+        'filename:http_proxies.txt': 'http.txt',
+        'filename:https_proxies.txt': 'https.txt',
+        'filename:socks4_proxies.txt': 'socks4.txt',
+        'filename:socks5_proxies.txt': 'socks5.txt'
+    }
+    for query, file_name in queries.items():
+        print(f'searching for {query}')
+        links_form_github: set = set(link for link in get_sources_from_github(10, query) if link)
+        path_to_http_sources = Path(PATH_TO_SOURCES, file_name)
+        save_iterable_to_file(path_to_http_sources, set(links_form_github))
 
     unchecked_proxies = get_uncheked_proxies()
     print(f'proxies were found {len(unchecked_proxies)}')
