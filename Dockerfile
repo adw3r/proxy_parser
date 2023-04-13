6ed9a933657a996bd8b488361dbb841a90d22118
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-04-13 17:17:31 +0300

    ...

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,13 @@ FROM python:latest
 
 WORKDIR /app
 
+RUN pip install -U pip
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-
 COPY . .
+
+COPY setup.py .
+RUN pip install -e .
+
+CMD python proxy_parser
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='proxy_parser',
+    version='',
+    packages=['tests', 'proxy_parser'],
+    url='',
+    license='',
+    author='Alexey Naidiuk',
+    author_email='',
+    description=''
+)
